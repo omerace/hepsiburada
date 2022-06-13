d356073a651a8cdd03cbcd472b042a5e073c9ef7
omerace <omerace@gmail.com>
2022-06-13 10:51:19 +0300

    Add project files.

---
+++ modum0/Program.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static modum0.hepsiburada;
+
+namespace modum0
+{
+    internal class Program
+    {
+        static readonly string inputRoverFile = @"C:\Users\omera\source\repos\modum0\modum0\inputRoverFile.txt";
+        static void Main(string[] args)
+        {
+            var (xBoundary, yBoundary, roverList) = PrepareInput();
+            var result = MarsRover(xBoundary, yBoundary, roverList);
+            writeOutput(result);
+        }
+
+        public static (int, int, List<Rover>) PrepareInput()
+        {
+            int x = 0;
+            int y = 0;
+            var roverList = new List<Rover>();
+
+            if (File.Exists(inputRoverFile))
+            {
+                using (StreamReader file = new StreamReader(inputRoverFile))
+                {
+                    int counter = 0;
+                    string ln;
+
+                    while ((ln = file.ReadLine()) != null)
+                    {
+                        ln = Regex.Replace(ln, @"[\r\n\t]+", "");
+                        Console.WriteLine(ln);
+                        if (counter==0)
+                        {
+                            try
+                            {
+                                x = int.Parse(ln.Split(" ")[0]);
+                                y = int.Parse(ln.Split(" ")[1]);
+                            }
+                            catch (Exception e)
+                            {
+                                throw new Exception("Invalid Boundaries. Exception Message: " + e.Message);
+                            }
+                        }
+                        else
+                        {
+                            var fLine = ln.Split(" ");
+                            ln = file.ReadLine();
+                            if (ln == null)
+                            {
+                                throw new Exception("Invalid Movements For Rover: " + counter);
+                            }
+                            var sLineTrimmed = ln.Trim();
+                            sLineTrimmed = Regex.Replace(sLineTrimmed, @"[\r\n\t ]+", "");
+                            Console.WriteLine(sLineTrimmed);
+                            var sLine = sLineTrimmed.Split(" ");
+                            var rover = new Rover();
+
+                            try
+                            {
+                                var position = new Position()
+                                {
+                                    X = int.Parse(fLine[0]),
+                                    Y = int.Parse(fLine[1])
+                                };
+                                if (fLine[2].Length > 1 || string.IsNullOrWhiteSpace(fLine[2]))
+                                {
+                                    throw new Exception("Invalid Face of Position For Rover: " + counter);
+                                }
+                                position.Face = Char.ToLower(fLine[2][0]);
+                                rover.Position = position;
+                            }
+                            catch (Exception)
+                            {
+                                throw new Exception("Invalid Positions For Rover: " + counter);
+                            }
+
+                            try
+                            {
+                                var movementString = sLine[0].ToLower();
+                                List<char> movements = new List<char>();
+                                movements.AddRange(movementString);
+                                rover.Movements = movements;
+                            }
+                            catch (Exception)
+                            {
+                                throw new Exception("Invalid Movements For Rover: " + counter);
+                            }
+
+                            roverList.Add(rover);
+                        }
+                        counter++;
+
+                    }
+                    file.Close();
+                    Console.WriteLine($"File has {counter-1} rovers.");
+                }
+            }
+            
+            return (x, y, roverList);
+        }
+
+        public static void writeOutput(List<Rover> rovers)
+        {
+            Console.WriteLine($"Final positions of the rovers: ");
+            foreach (var item in rovers)
+            {
+                Console.WriteLine(item.Position.X + " " + item.Position.Y + " " + char.ToUpperInvariant(item.Position.Face));
+            }
+        }
+    }
+}
---
+++ modum0/hepsiburada.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modum0
+{
+    public class hepsiburada
+    {
+        public class Rover
+        {
+            public Position Position { get; set; }
+            public List<char> Movements { get; set; }
+        }
+
+        public class Position
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public char Face { get; set; }
+        }
+
+        public static List<Rover> MarsRover(int xBoundary, int yBoundary, List<Rover> rovers)
+        {
+            var result = new List<string>();
+
+            foreach (var item in rovers.Select((rover, index) => (rover, index)))
+            {
+                if (item.rover.Position.X > xBoundary || item.rover.Position.Y > yBoundary)
+                {
+                    result.Add("Rover " + item.index + ". has invalid positions.");
+                    continue;
+                }
+
+                foreach (var move in item.rover.Movements)
+                {
+                    item.rover.Position = ChangePosition(item.rover.Position, Char.ToLower(move), xBoundary, yBoundary);
+                }
+            }
+            return rovers;
+        }
+
+        public static Position ChangePosition(Position oldPosiiton, char movement, int xLimit, int yLimit)
+        {
+            if (movement == 'l' || movement == 'r')
+            {
+                oldPosiiton.Face = TurnFlow(oldPosiiton.Face, movement);
+            } else if (movement == 'm')
+            {
+                if (oldPosiiton.Face == 'n')
+                {
+                    oldPosiiton.Y = oldPosiiton.Y == yLimit ? oldPosiiton.Y : oldPosiiton.Y + 1;
+                }
+                else if (oldPosiiton.Face == 'e')
+                {
+                    oldPosiiton.X = oldPosiiton.X == xLimit ? oldPosiiton.X : oldPosiiton.X + 1;
+                } 
+                else if (oldPosiiton.Face == 's')
+                {
+                    oldPosiiton.Y = oldPosiiton.Y == 0 ? oldPosiiton.Y : oldPosiiton.Y - 1;
+                } 
+                else if (oldPosiiton.Face == 'w')
+                {
+                    oldPosiiton.X = oldPosiiton.X == 0 ? oldPosiiton.X : oldPosiiton.X - 1;
+                }
+            }
+            return oldPosiiton;
+        }
+
+        public static char TurnFlow(char curDirection, char rotation)
+        {
+            if (curDirection == 'n' && rotation == 'l')
+            {
+                return 'w';
+            } else if (curDirection == 'n' && rotation == 'r')
+            {
+                return 'e';
+            } else if (curDirection == 'e' && rotation == 'l')
+            {
+                return 'n';
+            } else if (curDirection == 'e' && rotation == 'r')
+            {
+                return 's';
+            }
+            else if (curDirection == 's' && rotation == 'l')
+            {
+                return 'e';
+            }
+            else if (curDirection == 's' && rotation == 'r')
+            {
+                return 'w';
+            }
+            else if (curDirection == 'w' && rotation == 'l')
+            {
+                return 's';
+            }
+            else if (curDirection == 'w' && rotation == 'r')
+            {
+                return 'n';
+            } else
+            {
+                return curDirection;
+            }
+        }
+
+    }
+}
